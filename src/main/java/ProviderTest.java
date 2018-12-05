cc606764042a5a16bd9b48c397957f872ff296b2
1nfinity <e_jjk@163.com>
2018-12-05 17:11:30 +0800

    upload 181205

--- src/main/java/ProviderTest.java
+++ src/main/java/ProviderTest.java
@@ -1,12 +1,15 @@
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.ide.highlighter.JavaClassFileType;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiManager;
+import com.intellij.psi.*;
 import com.intellij.psi.impl.compiled.ClsFileImpl;
+import com.intellij.psi.impl.compiled.ClsMemberImpl;
+import com.intellij.psi.impl.source.PsiClassImpl;
+import com.intellij.psi.impl.source.PsiJavaFileImpl;
+import com.intellij.psi.impl.source.tree.java.JavaFileElement;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -17,13 +20,24 @@ import java.util.Collection;
  * @since 2018/12/4
  */
 public class ProviderTest extends RelatedItemLineMarkerProvider {
+    private int count = 0;
+
     @Override
     protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
         if (element instanceof PsiIdentifier) {
-            VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(new File("D:\\Study" +
-                    "\\Research\\SimpleTest\\out\\production\\SimpleTest\\Test.class"));
-            ClsFileImpl psiFile = (ClsFileImpl)PsiManager.getInstance(element.getProject()).findFile(virtualFile);
-            System.out.println("1");
+            if (element.getParent() instanceof PsiClassImpl) {
+                if (!(((PsiJavaFileImpl) (element.getParent().getParent())).getOriginalFile().getFileType() instanceof JavaClassFileType)) {
+                    VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(new File("D:\\Study" +
+                            "\\Research\\SimpleTest\\out\\production\\SimpleTest\\Test.class"));
+                    ClsFileImpl psiFile = (ClsFileImpl) PsiManager.getInstance(element.getProject()).findFile(virtualFile);
+                    PsiClass psiClass = (PsiClass) psiFile.getFirstChild();
+                    PsiIdentifier psiIdentifier = psiClass.getNameIdentifier();
+                    NavigationGutterIconBuilder<PsiElement> builder =
+                            NavigationGutterIconBuilder.create(TestIcon.FILE).setTarget(psiIdentifier).setTooltipText("Provider Test");
+                    result.add(builder.createLineMarkerInfo(element));
+                    System.out.println("1");
+                }
+            }
         }
     }
 }
---
+++ src/main/java/TestIcon.java
@@ -0,0 +1,11 @@
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author zou tairan
+ * @since 2018/12/5
+ */
+public class TestIcon {
+    public static final Icon FILE = IconLoader.getIcon("/jar-gray.png");
+}
